e659bb9bf9e85ec5719980bb56df063c1cd44bf0
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-05 20:54:40 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+// Conditional Ternary
+let isStuding = true;
+let subject = isStuding? "Maths" : "Computer";
+console.log(`I am studing ${subject}.`);
+
+let goingForShopping = false;
+let onWhichDay = goingForShopping? "monday" : "sunday";
+console.log(`we are going for shopping on ${onWhichDay}.`);
+
+let doingWork = "onComputer";
+let timeSpend = doingWork? "2 hours" : "3 hours";
+console.log(`He is doing work on computer for ${timeSpend}.`);
+
+let scoredMarks = 780;
+let totalMarks = scoredMarks? "780" : "110";
+console.log(`Ali's obtain marks in final is ${totalMarks}.`);
